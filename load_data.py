be50dd40cb7262c39ff70397800c2ebca24bd379
Jaebae <Chanont.W@hotmail.com>
2025-03-25 17:13:48 +0700

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import streamlit as st
+import os
 
 def load_default_data():
-    return pd.read_csv("Excel_functions_EN.csv")
+    csv_path = os.path.join(os.path.dirname(__file__), "Excel_functions_EN.csv")
+    return pd.read_csv(csv_path)
+
 
 def load_uploaded_data(uploaded_file):
     try:
